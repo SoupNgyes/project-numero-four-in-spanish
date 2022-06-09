03bb671d257678e3d9fd1e670e164b82a175be9a
fruitcake <80382564+SoupNgyes@users.noreply.github.com>
2022-06-09 13:24:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def add(num1, num2):
+  print(f"The sum is: {num1 + num2}")
+  print(f"The difference between the numbers is: {num1-num2}")
+
+
+
+num1 = int(input("Give me a number: "))
+num2 = int(input("Give me another number: "))
+add(num1, num2)
